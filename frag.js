2f8b0b4c7d12a201f39885e188d90e55560bda83
Che-Yu Wu <frank890417@gmail.com>
2022-07-30 15:51:56 -0400

    initial commint

---
+++ frag.js
@@ -0,0 +1,91 @@
+const frag = `
+	precision highp float;
+
+	uniform vec2 u_resolution;
+	uniform vec2 u_mouse;
+	uniform float u_time;
+	uniform vec3 u_lightDir;
+	uniform vec3 u_col;
+	uniform vec3 u_bgColor;
+	uniform mat3 uNormalMatrix;
+	uniform float u_pixelDensity;
+	uniform sampler2D u_tex;
+	uniform sampler2D u_canvas_tex; 
+
+	//attributes, in
+	varying vec4 var_centerGlPosition;
+	varying vec3 var_vertNormal;
+	varying vec2 var_vertTexCoord;
+
+	${frag_functions_default}
+
+	void main(){
+		vec2 st = var_vertTexCoord.xy /u_resolution.xy*u_resolution.y;
+		st.x*=u_resolution.x/u_resolution.y;
+		vec2 stBorder =st;
+		
+		stBorder.x+=cnoise(vec3(st*1000.,100.))/500.;
+		stBorder.y+=cnoise(vec3(st*1000.,1000.))/500.; 
+		
+		stBorder.x+=cnoise(vec3(st*100.,100.))/500.;
+		stBorder.y+=cnoise(vec3(st*100.,1000.))/500.; 
+
+
+
+	
+		vec3 canvasOffset = texture2D(u_canvas_tex,st* vec2(u_resolution.x/u_resolution.y,1.)).rgb;
+		st.x+=0.35/255.- canvasOffset.r/255.*3. ;
+		st.y+=0.35/255.- canvasOffset.g/255.*3.  ;
+		
+		float distortFactor =  0.8;
+		st.x+=  cnoise(vec3(st*2.,${(Math.random()*1000).toFixed(4)}))/(30.)*distortFactor  ;
+		st.y+= cnoise(vec3(st*20.,${(Math.random()*1000).toFixed(4)}))/(30.)*distortFactor 
+					+ cnoise(vec3(st/2.,${(Math.random()*1000).toFixed(4)}))/(100.)*distortFactor;
+		
+		// st.x+=cnoise(vec3(st*50.,100.))/300.  ;
+		// st.y+=cnoise(vec3(st*100.,1000.))/300.;
+		
+		vec4 texColor0 = texture2D(u_tex,st);
+		
+		//offset color Blocks
+		float offsetColor = 1./300.;
+		stBorder.x-= texColor0.r*offsetColor;
+		stBorder.y-=texColor0.g*offsetColor+ texColor0.b*offsetColor;
+		
+		vec4 texColor1 = texture2D(u_tex,st);
+		 
+		
+		vec2 st2 = st;
+		//brush feeling  
+		float brushFactor = 400.;
+		st2.x+=cnoise(vec3(st*1000.,100.))/brushFactor;
+		st2.y+=cnoise(vec3(st*1000.,1000.))/brushFactor; 
+ 
+		vec4 texColor2 = texture2D(u_tex,st2);
+		
+		float d = distance(vec2(0.5) ,st);  
+		// texColor*=1.-d+0.3;
+		// gl_FragColor= vec4(color,1.0)+texColor2; 
+		
+		float borderWidth = 20.;
+		bool isBorder = stBorder.x*u_resolution.x<borderWidth
+		|| (1.-stBorder.x)*u_resolution.x<borderWidth 
+		|| stBorder.y*u_resolution.y<borderWidth 
+		|| (1.-stBorder.y)*u_resolution.y<borderWidth;
+		
+		vec4 result ;
+		result = texColor1*0.8 + (texColor1*texColor2/1.9+texColor1/4.+texColor2/4.)/5.;
+		if ( isBorder){
+			result.rgb= vec3(u_bgColor);
+		} else {
+
+			// if ( distance(result.rgb, u_bgColor.rgb)<0.01 ){
+			// 	result.a=0.;
+			// }
+		} 
+		gl_FragColor =result;	
+	}
+`
+
+
+
---
+++ frag_texture.js
@@ -0,0 +1,53 @@
+const frag_texture = ` 
+	precision highp float;
+
+	uniform vec2 u_resolution;
+	uniform vec2 u_mouse;
+	uniform float u_time;
+	uniform vec3 u_lightDir;
+	uniform vec3 u_col;
+	uniform mat3 uNormalMatrix;
+	uniform float u_pixelDensity;
+	uniform sampler2D u_tex; 
+
+	//attributes, in
+	varying vec4 var_centerGlPosition;
+	varying vec3 var_vertNormal;
+	varying vec2 var_vertTexCoord; 
+
+	${frag_functions_default}
+
+	void main(){
+		vec2 st = var_vertTexCoord.xy /u_resolution.xy*min(u_resolution.x,u_resolution.y);
+		st.x*=u_resolution.x/u_resolution.y;
+			st.x*=u_resolution.x/u_resolution.y;
+			float sc = 1.*u_resolution.x/100.;
+			st*=sc;
+			st*=vec2(3.,1.);
+		
+			// st.y = 1.0 - st.y;
+			vec3 color = vec3(0.);
+			color+=cnoise(vec3(st*20.,10.))/7./3.;
+			color+=cnoise(vec3(st*30.,10.))/9./3.;
+			color+=cnoise(vec3(st*10.,10.))/9./3.;
+		
+			color+=cnoise(vec3(st*200.,10.))/8.;
+			color+=cnoise(vec3(st*300.,10.))/10.;
+			color+=cnoise(vec3(st*100.,10.))/10.;
+		
+			color+=cnoise(vec3(st*200.,10.))/8.;
+			color+=cnoise(vec3(st*300.,10.))/10.;
+			color+=cnoise(vec3(st*100.,10.))/10.;
+			color+=pNoise(st*10.,5)*pNoise(st*10.+2.,10)/2.;
+			color+=pNoise(st*30.,5)*pNoise(st*40.+2.,10)/2.; 
+			color/=2.;
+		
+			color = 1.-color;
+		
+		// vec3 color = vec3(st.x,st.y,1.);
+		gl_FragColor= vec4(color,1.0);
+	}
+`
+
+
+
